baa35c2a21e52ce9d036bd6b4e9d1e6a3ca37a91
Faris Syahluthfi <fariszero06@gmail.com>
2021-01-11 14:27:54 +0700

    Tugas UAS

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# UAS
+![Faris](foto/Faris.png)
 
 ## Nama: Faris Syahluthfi
 ## Kelas: TI.20.A.2
 ## NIM: 3120100034
+
+## UAS PEMROGRAMAN  <br>
---
+++ view/input_nilai.py
@@ -0,0 +1,14 @@
+from data import data
+
+while True:
+    print("")
+    c = input(" (T)ambah : ")
+    if c.lower() == 't':
+        print("=======Silahkan Diisi=======")
+        nama = input("Nama                :  ")
+        nim = input("Nim                 :  ")
+        tugas = int(input("Masukan Nilai Tugas :  "))
+        uts = int(input("Masukan Nilai UTS   :  "))
+        uas = int(input("Masukan Nilai UAS   :  "))
+        akhir = (0.30 * tugas) + (0.35 * uts) + (0.35 * uas)
+        data[nama] = nim, tugas, uts, uas, akhir
